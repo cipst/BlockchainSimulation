efecd3bb224d1fac5d93fa6b4c3a3c51aa9af12c
Stefano <stefano.cipolletta@outlook.it>
2021-12-20 17:55:24 +0100

    [utente] Oggetti IPC necessari + ricerca offset
    Ricerca del proprio PID all'interno dell'array 'users' per determinare l'offset dall'indirizzo 0 ('users')
    es.: figlio 0, pid 444 ==> (users + 0)->pid == 444 ==> offset 0
    
    figlio 1, pid 999 ==> (users + 1)->pid == 999 ==> offset 1
    
    ecc...

--- utente.c
+++ utente.c
@@ -1,37 +1,188 @@
 #include "master.h"
 
+/* #define DEBUG */
+
+void hdl(int, siginfo_t*, void*);
 /* Calcola il bilancio delle transazioni presenti nel Libro Mastro */
 int balanceFromLedger(ledger*);
+int whoAmI();
 
-int main(int argc, char** argv) {    
-    int balance = 0;
-    int ledgerId;
-    ledger* mastro;
-    int i, j;
-    int semId;
+struct sigaction act;
 
-    if ((semId = semget(ftok("../utils/private-key", 's'), 1, 0644)) < 0)
-        error("Semaphore get. (semget)");
+int semId;              /* ftok(..., 's') => 's': semaphore */
+int shmLedgerId;        /* ftok(..., 'l') => 'l': ledger */
+int shmUsersId;         /* ftok(..., 'u') => 'u': users */
+int shmNodesId;         /* ftok(..., 'n') => 'n': nodes */
+int shmActiveProcessId; /* ftok(..., 'p') => 'p': process */
 
-    if (reserveSem(semId, 0) < 0)
-        error("Semaphore reserve. (reserveSem)");
+ledger* mastro;     /* informazioni sul libro mastro in memoria condivisa */
+process* users;     /* informazioni sugli utenti in memoria condivisa */
+process* nodes;     /* informazione sui nodi in memoria condivisa */
+int* activeProcess; /* conteggio dei processi attivi in shared memory */
 
-    if ((ledgerId = shmget(ftok("../utils/private-key", 'l'), sizeof(ledger), 0644)) < 0)
-        error("Ledger ID. (shmget)");
+int usersNum;     /* numero di utenti nella simulazione */
+int nodesNum;     /* numero di nodi nella simulazione */
+int budgetInit;   /* budget iniziale di un utente */
+int reward;       /* reward da applicare alla transazione */
+long minTransGen; /* minimo valore del tempo (espresso in nanosecondi) che trascorre fra la generazione di una transazione e la seguente da parte di un utente */
+long maxTransGen; /* massimo valore del tempo (espresso in nanosecondi) che trascorre fra la generazione di una transazione e la seguente da parte di un utente */
+int retry;        /* numero massimo di fallimenti consecutivi nella generazione di transazioni dopo cui un processo utente termina */
 
-    mastro = (ledger*)shmat(ledgerId, NULL, 0);
-    if (mastro == (void*)-1)
-        error("Ledger attaching. (shmat)");
+int offset;
+int balance = 0;
+int i, j;
 
-    balance = balanceFromLedger(mastro);
+int main(int argc, char** argv) {
+    bzero(&act, sizeof(act)); /* bzero setta tutti i bytes a zero */
+
+    /* Dico a sigaction (act) quale handler deve mandare in esecuzione */
+    act.sa_sigaction = &hdl;
+    /* Il campo SA_SIGINFO flag indica a sigaction() di usare il capo sa_sigaction, e non sa_handler. */
+    act.sa_flags = SA_SIGINFO;
+
+    /* SEGNALI */
+    if (sigaction(SIGINT, &act, NULL) < 0) {
+        perror(RED "Sigaction: Failed to assign SIGINT to custom handler" WHITE);
+        exit(EXIT_FAILURE);
+    }
+    if (sigaction(SIGTERM, &act, NULL) < 0) {
+        perror(RED "Sigaction: Failed to assign SIGTERM to custom handler" WHITE);
+        exit(EXIT_FAILURE);
+    }
+    if (sigaction(SIGSEGV, &act, NULL) < 0) {
+        perror(RED "Sigaction: Failed to assign SIGSEGV to custom handler" WHITE);
+        exit(EXIT_FAILURE);
+    }
+    if (sigaction(SIGUSR1, &act, NULL) < 0) {
+        perror(RED "Sigaction: Failed to assign SIGUSR1 to custom handler" WHITE);
+        exit(EXIT_FAILURE);
+    }
+
+    /* SEMAFORI */
+    if ((semId = semget(ftok("./utils/private-key", 's'), 3, IPC_CREAT | 0644)) < 0) {
+        perror(RED "Semaphore pool creation failure" WHITE);
+        exit(EXIT_FAILURE);
+    }
+
+    /* MEMORIE CONDIVISE */
+    if ((shmLedgerId = shmget(ftok("./utils/private-key", 'l'), sizeof(ledger), IPC_CREAT | 0644)) < 0) {
+        perror(RED "Shared Memory creation failure [LEDGER]" WHITE);
+        exit(EXIT_FAILURE);
+    }
+
+    mastro = (ledger*)shmat(shmLedgerId, NULL, 0);
+    if (mastro == (void*)-1) {
+        perror(RED "Shared Memory attach failure [LEDGER]" WHITE);
+        exit(EXIT_FAILURE);
+    }
+
+    usersNum = atoi(argv[1]);
+    if ((shmUsersId = shmget(ftok("./utils/private-key", 'u'), sizeof(process) * usersNum, IPC_CREAT | 0644)) < 0) {
+        perror(RED "Shared Memory creation failure [USERS]" WHITE);
+        exit(EXIT_FAILURE);
+    }
+
+    users = (process*)shmat(shmUsersId, NULL, 0);
+    if (users == (void*)-1) {
+        perror(RED "Shared Memory attach failure [USERS]" WHITE);
+        exit(EXIT_FAILURE);
+    }
+
+    nodesNum = atoi(argv[2]);
+    if ((shmNodesId = shmget(ftok("./utils/private-key", 'n'), sizeof(process) * nodesNum, IPC_CREAT | 0644)) < 0) {
+        perror(RED "Shared Memory creation failure [NODES]" WHITE);
+        exit(EXIT_FAILURE);
+    }
+
+    nodes = (process*)shmat(shmNodesId, NULL, 0);
+    if (nodes == (void*)-1) {
+        perror(RED "Shared Memory attach failure [NODES]" WHITE);
+        exit(EXIT_FAILURE);
+    }
+
+    /* per sincronizzare i processi alla creazione iniziale, e dare loro il via con le operazioni */
+    shmActiveProcessId = shmget(ftok("./utils/key-private", 'f'), sizeof(int), IPC_CREAT | 0644);
+    if (shmActiveProcessId < 0) {
+        perror(RED "Shared Memory creation failure [ActiveProcess]" WHITE);
+        exit(EXIT_FAILURE);
+    }
+
+    activeProcess = (int*)shmat(shmActiveProcessId, NULL, 0);
+    if (activeProcess == (void*)-1) {
+        perror(RED "Shared Memory attach failure [ActiveProcess]" WHITE);
+        exit(EXIT_FAILURE);
+    }
+
+    /* Il semaforo 2 viene utilizzato per accedere in mutua esclusione all'area di shared memory per sincronizzarsi */
+    reserveSem(semId, 2);
+    printf("Processi attivi: %d\n", *activeProcess);
+    (*activeProcess)++;
+    releaseSem(semId, 2);
+
+#ifdef DEBUG
+    printf("[%s%d%s] USER - Wrote +1 in shared\n", CYAN, getpid(), WHITE);
+#endif
+
+    /*  balance = balanceFromLedger(mastro);
     printf("[%d] BILANCIO: %d\n", getpid(), balance);
 
     if (shmdt(mastro) < 0)
-        error("Ledger detaching. (shmdt)");
+        error("Ledger detaching. (shmdt)"); */
+
+    reserveSem(semId, 0); /* aspetto il via dal master */
+
+    if ((offset = whoAmI()) < 0) {
+        printf("%sUser %d not found!%s\n", RED, getpid(), WHITE);
+    }
+
+    releaseSem(semId, 0);
 
     printf("\t%d Termino...\n", getpid());
 
     exit(EXIT_SUCCESS);
+}
+
+int whoAmI() {
+    int found = -1;
+    int i;
+    for (i = 0; found == -1 && i < usersNum; ++i) {
+        if ((users + i)->pid == getpid()) {
+            found = i;
+        }
+    }
+    return found;
+}
+
+void hdl(int sig, siginfo_t* siginfo, void* context) {
+    /**
+	 * 	Questo è l'handler dei segnali, gestisce i segnali:
+	 *  	-	SIGINT
+	 *  	-	SIGTERM
+	 *  	-	SIGSEGV
+	 **/
+
+    if (sig == SIGINT) {
+        releaseSem(semId, 0);
+        shmdt(mastro);
+        shmdt(users);
+        shmdt(nodes);
+        shmdt(activeProcess);
+        exit(EXIT_SUCCESS);
+    } else if (sig == SIGTERM) {
+        releaseSem(semId, 0);
+        shmdt(mastro);
+        shmdt(users);
+        shmdt(nodes);
+        shmdt(activeProcess);
+        exit(EXIT_FAILURE);
+    } else if (sig == SIGSEGV) {
+        releaseSem(semId, 0);
+        shmdt(mastro);
+        shmdt(users);
+        shmdt(nodes);
+        shmdt(activeProcess);
+        error("SEGMENTATION VIOLATION [USER]");
+    }
 }
 
 int balanceFromLedger(ledger* l) {
