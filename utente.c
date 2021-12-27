181490cad73b28017274406ea33231a0fd875ca3
Stefano <stefano.cipolletta@outlook.it>
2021-12-27 21:09:45 +0100

    [utente] Coda di messaggi + SIGUSR2

--- utente.c
+++ utente.c
@@ -32,17 +32,17 @@ struct sigaction act;
 struct timespec tp;
 struct timespec remaining, request;
 
-int semId;              /* ftok(..., 's') => 's': semaphore */
-int queueId;            /* ftok(..., 'q') => 'q': queue */
-int shmLedgerId;        /* ftok(..., 'l') => 'l': ledger */
-int shmUsersId;         /* ftok(..., 'u') => 'u': users */
-int shmNodesId;         /* ftok(..., 'n') => 'n': nodes */
-int shmActiveProcessId; /* ftok(..., 'p') => 'p': process */
+int semId;            /* ftok(..., 's') => 's': semaphore */
+int queueId;          /* ftok(..., 'q') => 'q': queue */
+int shmLedgerId;      /* ftok(..., 'l') => 'l': ledger */
+int shmUsersId;       /* ftok(..., 'u') => 'u': users */
+int shmNodesId;       /* ftok(..., 'n') => 'n': nodes */
+int shmActiveUsersId; /* ftok(..., 'a') => 'a': process */
 
-ledger* mastro;     /* informazioni sul libro mastro in memoria condivisa */
-process* users;     /* informazioni sugli utenti in memoria condivisa */
-process* nodes;     /* informazione sui nodi in memoria condivisa */
-int* activeProcess; /* conteggio dei processi attivi in shared memory */
+ledger* mastro;   /* informazioni sul libro mastro in memoria condivisa */
+process* users;   /* informazioni sugli utenti in memoria condivisa */
+process* nodes;   /* informazione sui nodi in memoria condivisa */
+int* activeUsers; /* conteggio dei processi attivi in shared memory */
 
 int SO_USERS_NUM;           /* numero di utenti nella simulazione */
 int SO_NODES_NUM;           /* numero di nodi nella simulazione */
@@ -54,9 +54,19 @@ int SO_RETRY;               /* numero massimo di fallimenti consecutivi nella ge
 int offset;                 /* offset per la memoria condivisa 'users', ogni utente è a conoscenza della sua posizione in 'users' */
 
 int balance = 0;
-int i, j;
+int i, j, try = 0;
 
 int main(int argc, char** argv) {
+    /* Salvo le info necessarie per l'utente */
+    SO_USERS_NUM = atoi(argv[1]);
+    SO_NODES_NUM = atoi(argv[2]);
+    SO_BUDGET_INIT = atoi(argv[3]);
+    SO_REWARD = atoi(argv[4]);
+    SO_MIN_TRANS_GEN_NSEC = atol(argv[5]);
+    SO_MAX_TRANS_GEN_NSEC = atol(argv[6]);
+    SO_RETRY = atoi(argv[7]);
+    offset = atoi(argv[8]);
+
     bzero(&act, sizeof(act)); /* bzero setta tutti i bytes a zero */
 
     /* Dico a sigaction (act) quale handler deve mandare in esecuzione */
@@ -81,16 +91,14 @@ int main(int argc, char** argv) {
         perror(RED "[USER] Sigaction: Failed to assign SIGUSR1 to custom handler" WHITE);
         exit(EXIT_FAILURE);
     }
+    if (sigaction(SIGUSR2, &act, NULL) < 0) {
+        perror(RED "[USER] Sigaction: Failed to assign SIGUSR2 to custom handler" WHITE);
+        exit(EXIT_FAILURE);
+    }
 
     /* »»»»»»»»»» SEMAFORI »»»»»»»»»» */
     if ((semId = semget(ftok("./utils/private-key", 's'), 3, IPC_CREAT | 0400 | 0200 | 040 | 020)) < 0) {
         perror(RED "[USER] Semaphore pool creation failure" WHITE);
-        exit(EXIT_FAILURE);
-    }
-
-    /* »»»»»»»»»» CODA DI MESSAGGI »»»»»»»»»» */
-    if ((queueId = msgget(ftok("./utils/private-key", 'q'), IPC_CREAT | IPC_EXCL | 0400 | 0200 | 040 | 020)) < 0) {
-        perror(RED "[USER] Message Queue creation failure" WHITE);
         exit(EXIT_FAILURE);
     }
 
@@ -106,7 +114,6 @@ int main(int argc, char** argv) {
         exit(EXIT_FAILURE);
     }
 
-    SO_USERS_NUM = atoi(argv[1]);
     if ((shmUsersId = shmget(ftok("./utils/private-key", 'u'), sizeof(process) * SO_USERS_NUM, IPC_CREAT | 0400 | 0200 | 040 | 020)) < 0) {
         perror(RED "[USER] Shared Memory creation failure [USERS]" WHITE);
         exit(EXIT_FAILURE);
@@ -118,7 +125,6 @@ int main(int argc, char** argv) {
         exit(EXIT_FAILURE);
     }
 
-    SO_NODES_NUM = atoi(argv[2]);
     if ((shmNodesId = shmget(ftok("./utils/private-key", 'n'), sizeof(process) * SO_NODES_NUM, IPC_CREAT | 0400 | 0200 | 040 | 020)) < 0) {
         perror(RED "[USER] Shared Memory creation failure [NODES]" WHITE);
         exit(EXIT_FAILURE);
@@ -131,22 +137,17 @@ int main(int argc, char** argv) {
     }
 
     /* per sincronizzare i processi alla creazione iniziale, e dare loro il via con le operazioni */
-    shmActiveProcessId = shmget(ftok("./utils/key-private", 'f'), sizeof(int), IPC_CREAT | 0644);
-    if (shmActiveProcessId < 0) {
+    shmActiveUsersId = shmget(ftok("./utils/key-private", 'a'), sizeof(int), IPC_CREAT | 0644);
+    if (shmActiveUsersId < 0) {
         perror(RED "[USER] Shared Memory creation failure [ActiveProcess]" WHITE);
         exit(EXIT_FAILURE);
     }
 
-    activeProcess = (int*)shmat(shmActiveProcessId, NULL, 0);
-    if (activeProcess == (void*)-1) {
+    activeUsers = (int*)shmat(shmActiveUsersId, NULL, 0);
+    if (activeUsers == (void*)-1) {
         perror(RED "[USER] Shared Memory attach failure [ActiveProcess]" WHITE);
         exit(EXIT_FAILURE);
     }
-
-    /* Il semaforo 2 viene utilizzato per accedere in mutua esclusione all'area di shared memory per sincronizzarsi */
-    reserveSem(semId, 2);
-    (*activeProcess)++;
-    releaseSem(semId, 2);
 
 #ifdef DEBUG
     reserveSem(semId, 1);
@@ -154,13 +155,10 @@ int main(int argc, char** argv) {
     releaseSem(semId, 1);
 #endif
 
-    /* Salvo le info necessarie per l'utente */
-    SO_BUDGET_INIT = atoi(argv[3]);
-    SO_REWARD = atoi(argv[4]);
-    SO_MIN_TRANS_GEN_NSEC = atol(argv[5]);
-    SO_MAX_TRANS_GEN_NSEC = atol(argv[6]);
-    SO_RETRY = atoi(argv[7]);
-    offset = atoi(argv[8]);
+    /* Il semaforo 2 viene utilizzato per accedere in mutua esclusione all'area di shared memory per sincronizzarsi */
+    reserveSem(semId, 2);
+    (*activeUsers)++;
+    releaseSem(semId, 2);
 
     srand(time(NULL) % getpid()); /* randomize seed */
 
@@ -173,8 +171,12 @@ int main(int argc, char** argv) {
         if (balance >= 2) {
             transaction trans;
             trans = createTransaction();
-
-            /* INVIO DELLA TRANSAZIONE */
+#ifdef DEBUG
+            reserveSem(semId, 1);
+            printTransaction(trans);
+            releaseSem(semId, 1);
+#endif
+            sendTransaction(trans);
 
             sleepTransactionGen();
         }
@@ -183,33 +185,6 @@ int main(int argc, char** argv) {
     printf("\t%d Termino...\n", getpid());
 
     exit(EXIT_SUCCESS);
-}
-
-void sleepTransactionGen() {
-    long transGenNsec, transGenSec;
-
-    transGenNsec = (rand() % (SO_MAX_TRANS_GEN_NSEC - SO_MIN_TRANS_GEN_NSEC + 1)) + SO_MIN_TRANS_GEN_NSEC;
-
-    if (transGenNsec > 999999999) {
-        transGenSec = transGenNsec / 1000000000;    /* trovo la parte in secondi */
-        transGenNsec -= (transGenSec * 1000000000); /* tolgo la parte in secondi dai nanosecondi */
-    }
-
-#ifdef DEBUG
-    reserveSem(semId, 1);
-    printf("%sSleep for: %lds %ldnsec\n%s", RED, transGenSec, transGenNsec, WHITE);
-    releaseSem(semId, 1);
-#endif
-
-    request.tv_nsec = transGenNsec;
-    request.tv_sec = transGenSec;
-    remaining.tv_nsec = transGenNsec;
-    remaining.tv_sec = transGenSec;
-
-    if (nanosleep(&request, &remaining) < 0) {
-        perror(RED "[USER] Nanosleep failed" WHITE);
-        exit(EXIT_FAILURE);
-    }
 }
 
 void hdl(int sig, siginfo_t* siginfo, void* context) {
@@ -226,7 +201,7 @@ void hdl(int sig, siginfo_t* siginfo, void* context) {
             shmdt(mastro);
             shmdt(users);
             shmdt(nodes);
-            shmdt(activeProcess);
+            shmdt(activeUsers);
             exit(EXIT_SUCCESS);
 
         case SIGTERM:
@@ -234,7 +209,7 @@ void hdl(int sig, siginfo_t* siginfo, void* context) {
             shmdt(mastro);
             shmdt(users);
             shmdt(nodes);
-            shmdt(activeProcess);
+            shmdt(activeUsers);
             exit(EXIT_FAILURE);
 
         case SIGSEGV:
@@ -242,7 +217,7 @@ void hdl(int sig, siginfo_t* siginfo, void* context) {
             shmdt(mastro);
             shmdt(users);
             shmdt(nodes);
-            shmdt(activeProcess);
+            shmdt(activeUsers);
             error("SEGMENTATION VIOLATION [USER]");
 
         case SIGUSR1: {
@@ -254,35 +229,61 @@ void hdl(int sig, siginfo_t* siginfo, void* context) {
             sendTransaction(trans);
             break;
         }
+
+        case SIGUSR2: {
+            reserveSem(semId, 1);
+            printf("\n[ %sSIGUSR2%s ] Impossible to send transaction to\n", YELLOW, WHITE);
+            releaseSem(semId, 1);
+
+            if (try == SO_RETRY) { /* non è riuscito ad inviare la transazione */
+                reserveSem(semId, 1);
+                printf("[ %s%d%s ] Failed to send transaction for SO_RETRY times\n", CYAN, getpid(), WHITE);
+                releaseSem(semId, 1);
+
+                shmdt(mastro);
+                shmdt(users);
+                shmdt(nodes);
+                shmdt(activeUsers);
+
+                exit(EXIT_FAILURE);
+            }
+            break;
+        }
     }
 }
 
 void sendTransaction(transaction trans) {
     int nodeReceiver, i = 0;
-    message* mex;
+    message msg;
+    int amount = trans.quantity;
+    int reward = trans.reward;
 
     nodeReceiver = (rand() % SO_NODES_NUM); /* estraggo randomicamente una posizione tra 0 e SO_NODES_NUM escluso */
 
-    mex->node = (nodes + nodeReceiver)->pid;
-    mex->transaction = trans;
-
-    while (i < SO_RETRY) {
-        if (msgsnd(queueId, mex, sizeof(transaction) - sizeof(pid_t), 0) < 0) {
-            ++i;
-            /* perror(RED "[USER] Error in msgsnd()" WHITE);
-            exit(EXIT_FAILURE); */
-        }
-    }
-
-    if (i == SO_RETRY) { /* non è riuscito ad inviare la transazione */
-        releaseSem(semId, 0);
-        kill(getppid(), SIGUSR1);
-        shmdt(mastro);
-        shmdt(users);
-        shmdt(nodes);
-        shmdt(activeProcess);
+    /* »»»»»»»»»» CODA DI MESSAGGI »»»»»»»»»» */
+    /* in base al nodo estratto (nodeReceiver) scelgo la coda di messaggi di quel nodo */
+    if ((queueId = msgget(ftok("./utils/private-key", 'q'), IPC_EXCL | 0400 | 0200 | 040 | 020)) < 0) {
+        perror(RED "[USER] Message Queue creation failure" WHITE);
         exit(EXIT_FAILURE);
     }
+
+    msg.mtype = (nodes + nodeReceiver)->pid;
+    msg.transaction = trans;
+
+    printf("INVIO...\n");
+
+    if (msgsnd(queueId, &msg, sizeof(msg) - sizeof(pid_t), IPC_NOWAIT) < 0) {
+        perror(RED "[USER] Error in msgsnd()" WHITE);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("INVIATO!\n", try);
+
+    /* TOFIX */
+
+    reserveSem(semId, 0);
+    (users + offset)->balance -= (amount - reward);
+    releaseSem(semId, 0);
 }
 
 int balanceFromLedger() {
@@ -328,9 +329,32 @@ transaction createTransaction() {
     releaseSem(semId, 1);
 #endif
 
-    reserveSem(semId, 0);
-    (users + offset)->balance -= (amount - nodeReward);
-    releaseSem(semId, 0);
-
     return trans;
+}
+
+void sleepTransactionGen() {
+    long transGenNsec, transGenSec;
+
+    transGenNsec = (rand() % (SO_MAX_TRANS_GEN_NSEC - SO_MIN_TRANS_GEN_NSEC + 1)) + SO_MIN_TRANS_GEN_NSEC;
+
+    if (transGenNsec > 999999999) {
+        transGenSec = transGenNsec / 1000000000;    /* trovo la parte in secondi */
+        transGenNsec -= (transGenSec * 1000000000); /* tolgo la parte in secondi dai nanosecondi */
+    }
+
+#ifdef DEBUG
+    reserveSem(semId, 1);
+    printf("%sSleep for: %lds %ldnsec\n%s", RED, transGenSec, transGenNsec, WHITE);
+    releaseSem(semId, 1);
+#endif
+
+    request.tv_nsec = transGenNsec;
+    request.tv_sec = transGenSec;
+    remaining.tv_nsec = transGenNsec;
+    remaining.tv_sec = transGenSec;
+
+    if (nanosleep(&request, &remaining) < 0) {
+        perror(RED "[USER] Nanosleep failed" WHITE);
+        exit(EXIT_FAILURE);
+    }
 }
